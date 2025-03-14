5783ae67d487e499d4e7f0d3310d2289c08a4426
Koss86 <jacobdelarosa123@gmail.com>
2025-03-14 01:26:33 -0500

    Day2 Part 1 success

--- day2.c
+++ day2.c
@@ -3,6 +3,12 @@
 #include <string.h>
 
 #define DB_SIZE 2500
+#define ROCK 'A'
+#define PAPER 'B'
+#define SCISSORS 'C'
+#define P_ROCK 'X' 
+#define P_PAPER 'Y' 
+#define P_SCISSORS 'Z'
 
 int main () {
     FILE *file;
@@ -10,5 +16,39 @@ int main () {
         printf("Unable to read file.\n");
         return -1;
     }
-    
+    //  1 = ROCK 2 = PAPER 3 = SCISSORS
+    //  0 = Lost 3 = DRAW  6 = WON
+
+    int score = 0;
+    char opponent, player;
+
+    while (fscanf(file, "%c %c\n", &opponent, &player) != EOF) {
+        if (opponent == ROCK) {
+            if (player == P_ROCK) {
+                score += 4;
+            } else if (player == P_PAPER) {
+                score += 8;
+            } else {
+                score += 3;
+            }
+        } else if (opponent == PAPER) {
+            if (player == P_ROCK) {
+                score += 1;
+            } else if (player == P_PAPER) {
+                score += 5;
+            } else {
+                score += 9;
+            }
+        } else {
+            if (player == P_ROCK) {
+                score += 7;
+            } else if (player == P_PAPER) {
+                score += 2;
+            } else {
+                score += 6;
+            }
+        }
+    }
+    printf("Part 1 answer: %i\n", score);
+    return 0;
 }
