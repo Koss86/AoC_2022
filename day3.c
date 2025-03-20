71461d1e1eb6a8f5aa7337e37e466669d4cdcf55
Koss86 <jacobdelarosa123@gmail.com>
2025-03-20 08:08:52 -0500

    Success on Part 2 of Day 3.

--- day3.c
+++ day3.c
@@ -3,11 +3,18 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define DB_SIZE 300
 #define BUFF_SIZE 60
+
+typedef struct Rucks {
+    char *str1;
+    char *str2;
+    char *str3;
+}Rucks;
 
 void split_even_string(int, char *, char *);
 bool char_in_str(char, char *);
-int return_priority(char c);
+int return_priority(char);
 
 int main() {
     FILE *file;
@@ -21,36 +28,92 @@ int main() {
         return -1;
     }
     char *buff2;
-    if ((buff2 = malloc((BUFF_SIZE/2) * sizeof(char))) == NULL) {
+    if ((buff2 = malloc((BUFF_SIZE) * sizeof(char))) == NULL) {
         printf("Error. Malloc failed.\n");
         return -1;
     }
+    Rucks *rucksacks;
+    if ((rucksacks = malloc(DB_SIZE*sizeof(Rucks))) == NULL) {
+        printf("Error. Malloc failed.\n");
+        return -1;
+    }
+    
     memset(buff1, 0, BUFF_SIZE * sizeof(char));
-    memset(buff2, 0, (BUFF_SIZE/2) * sizeof(char));
+    memset(buff2, 0, BUFF_SIZE * sizeof(char));
 
-    int sum1 = 0;
+    int sum = 0;
+    int indx = 0;
+    int counter = 0;
     while (fscanf(file, "%s\n", buff1) != EOF) {
         int leng = strlen(buff1);
+        if (counter == 0) {
+            if ((rucksacks[indx].str1 = malloc(BUFF_SIZE*sizeof(char))) == NULL) {
+                printf("Error. Malloc faild in rucksacks at %i\n", indx);
+                return -1;
+            } else {
+                memset(rucksacks[indx].str1, 0, BUFF_SIZE*sizeof(char));
+                strncpy(rucksacks[indx].str1, buff1, leng);
+                counter++;
+            }
+        } else if (counter == 1) {
+            if ((rucksacks[indx].str2 = malloc(BUFF_SIZE*sizeof(char))) == NULL) {
+                printf("Error. Malloc faild in rucksacks at %i\n", indx);
+                return -1;
+            } else {
+                memset(rucksacks[indx].str2, 0, BUFF_SIZE*sizeof(char));
+                strncpy(rucksacks[indx].str2, buff1, leng);
+                counter++;
+            }
+        } else if (counter == 2) {
+            if ((rucksacks[indx].str3 = malloc(BUFF_SIZE*sizeof(char))) == NULL) {
+                printf("Error. Malloc faild in rucksacks at %i\n", indx);
+                return -1;
+            } else {
+                memset(rucksacks[indx].str3, 0, BUFF_SIZE*sizeof(char));
+                strncpy(rucksacks[indx++].str3, buff1, leng);
+                counter = 0;
+            }
+        }
+        
         split_even_string(leng, buff1, buff2);
         leng = leng/2;
+        
         for (int i = 0; i < leng; i++) {
             char tmp = buff1[i];
             if (char_in_str(tmp, buff2) == true) {
-                sum1 += return_priority(tmp);
+                sum += return_priority(tmp);
                 break;
             }
         }
     }
-    printf("Part 1 answer: %i\n", sum1);
-    free(buff1); free(buff2);
+    printf("Part 1 answer: %i\n", sum);
+    fclose(file);
+    sum = 0;
+    for (int i = 0; i < DB_SIZE/3; i++) {
+        int leng = strlen(rucksacks[i].str1);
+        for (int j = 0; j < leng; j++) {
+            char tmp = rucksacks[i].str1[j];
+            if (char_in_str(tmp, rucksacks[i].str2) == true) {
+                if (char_in_str(tmp, rucksacks[i].str3) == true) {
+                    sum += return_priority(tmp);
+                    break;
+                }
+            }
+        }
+    }
+    printf("Part 2 answer: %i\n", sum);
+    free(buff1); free(buff2); 
     return 0;
 }
-void split_even_string(int leng, char *s1, char *s2) {
-    for (int i = 0, j = leng/2; j < leng; i++, j++) {
-        s2[i] = s1[j];
-        s2[i+1] = '\0';
+void split_even_string(int leng, char *src, char *dest) {
+    if (leng % 2 != 0) {
+        return;
     }
-    s1[leng/2] = '\0';
+    for (int i = 0, j = leng/2; j < leng; i++, j++) {
+        dest[i] = src[j];
+        dest[i+1] = '\0';
+    }
+    src[leng/2] = '\0';
 }
 bool char_in_str(char c, char *s) {
     int leng = strlen(s);
