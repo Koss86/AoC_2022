99eff32edaaa55456aa4e533950ac6812f63d782
Koss86 <jacobdelarosa123@gmail.com>
2025-03-20 10:22:11 -0500

    Day 4 Part 1 Success.

--- day4.c
+++ day4.c
@@ -5,8 +5,19 @@
 
 int main() {
     FILE *file;
-    if ((file = "inputs/input4.txt") == NULL) {
+    if ((file = fopen("inputs/input4.txt", "r")) == NULL) {
         printf("Unable to open file.\n");
         return -1;
     }
+    int elf1[2];
+    int elf2[2];
+    int overlap = 0;
+    int enclosed = 0;
+    while (fscanf(file, "%i-%i,%i-%i\n", &elf1[0], &elf1[1], &elf2[0], &elf2[1]) != EOF) {
+        if (elf1[0] >= elf2[0] && elf1[1] <= elf2[1] ||
+            elf2[0] >= elf1[0] && elf2[1] <= elf1[1]) {
+                enclosed++;
+        }
+    }
+    printf("Part 1 answer: %i\n", enclosed);
 }
