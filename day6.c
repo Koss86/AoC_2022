4c17e2eca7c196f4fd7be0c2ab2f53f8de98a1e7
Koss86 <jacobdelarosa123@gmail.com>
2025-03-24 05:22:44 -0500

    Cleaned up Day 6 a bit.

--- day6.c
+++ day6.c
@@ -18,39 +18,31 @@ int main() {
         printf("Malloc failed.\n");
  
     }
-    
-    char c;
-    int indx = 0;
-
-    while ((c = fgetc(file)) != EOF) { buff[indx++] = c; }
-    
-    char c1, c2, c3, c4;
-    int leng = strlen(buff);
-
-    for (int i = 0; i < leng; i++) {
-        c1 = buff[i]; c2 = buff[i+1];
-        c3 = buff[i+2]; c4 = buff[i+3];
-
-        if (c1 != c2 && c1 != c3 && c1 != c4) {
-            if (c2 != c3 && c2 != c4) {
-                if (c3 != c4) {
-                    printf("Part 1 answer: %i\n", i+4);
-                    break;
-                }
-            }
-        } 
-    }
-
     char *s;
     if ((s = malloc(15*sizeof(char))) == NULL) {
         printf("Malloc failed.\n");
         return -1;
     }
+    char c;
+    int indx = 0;
 
-    for (int i = 0; i < leng-1; i += 14) {
+    while ((c = fgetc(file)) != EOF) { buff[indx++] = c; }
+    
+    int leng = strlen(buff);
+
+    for (int i = 0; i < leng-1; i++) {
+        strncpy(s, &buff[i], 4);
+        if (are_unique(s)) {
+            printf("Part 1 answer: %i\n", i+4);
+            break;
+        }
+    }
+
+
+    for (int i = 0; i < leng-1; i++) {
         strncpy(s, buff+i, 14);
-        if (are_unique(s) == true) {
-            printf("Part 2 answer: %i\n", i+13);
+        if (are_unique(s)) {
+            printf("Part 2 answer: %i\n", i+14);
             break;
         }
     }
@@ -62,7 +54,7 @@ bool are_unique(char *s) {
     int leng = strlen(s);
     for (int i = 0; i < leng-1; i++) {
         char c1 = s[i];
-        for (int j = i+1; j < leng-1; j++) {
+        for (int j = i+1; j < leng; j++) {
             char c2 = s[j];
             if (c1 == c2) {
                 return false;
