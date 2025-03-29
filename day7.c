a71778e57e8b51061fb8d76a07ebb4d955e6ff13
Koss86 <jacobdelarosa123@gmail.com>
2025-03-28 21:42:25 -0500

    misc

--- day7.c
+++ day7.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <string.h>
 
 int main() {
     FILE *file;
@@ -9,5 +8,6 @@ int main() {
         printf("Unable to open file.\n");
         return -1;
     }
-    
+    char buffer[256];
+   memset(buffer, 0, 256*sizeof(char)); 
 }
