082a87ec79e2eb3489c2df331a592925baeba27a
Koss86 <jacobdelarosa123@gmail.com>
2025-04-20 05:21:44 -0500

    misc

--- day8.c
+++ day8.c
@@ -34,6 +34,8 @@ int main(void) {
     } else if (c == '\n') {
       indx1++;   // Icrement indx1 to move to next row,
       indx2 = 0; // and reset indx2 to start at beginning of col.
+    } else {
+      continue;
     }
   }
   int boarder_trees = (NUM_ROWS * 2) + (NUM_ROWS - 2) * 2;
@@ -42,7 +44,8 @@ int main(void) {
   // NUM_ROWS-1 to skip last row/col
   for (int i = 1; i < NUM_ROWS - 1; i++) {
     for (int j = 1; j < NUM_ROWS - 1; j++) {
-      printf("----searching row %i col %i-----\n%15i\n", i, j, row[i].col[j]);
+      // printf("----searching row %i col %i-----\n%15i\n", i, j,
+      // row[i].col[j]);
       if (is_visable(row, i, j) == true) {
         visable++;
       }
@@ -71,7 +74,7 @@ bool is_visable(Field *field, int row, int col) {
   for (int i = 0; i < col; i++) {
     if (field[row].col[i] >= tree_size) {
       taller++;
-      printf("tree size: %i taller: %i\n", tree_size, field[row].col[i]);
+      // printf("tree size: %i taller: %i\n", tree_size, field[row].col[i]);
     }
   }
 
@@ -84,7 +87,7 @@ bool is_visable(Field *field, int row, int col) {
   for (int i = col + 1; i < NUM_ROWS; i++) {
     if (field[row].col[i] >= tree_size) {
       taller++;
-      printf("tree size: %i taller: %i\n", tree_size, field[row].col[i]);
+      // printf("tree size: %i taller: %i\n", tree_size, field[row].col[i]);
     }
   }
 
@@ -97,7 +100,7 @@ bool is_visable(Field *field, int row, int col) {
   for (int i = 0; i < row; i++) {
     if (field[i].col[col] >= tree_size) {
       taller++;
-      printf("tree size: %i taller: %i\n", tree_size, field[i].col[col]);
+      // printf("tree size: %i taller: %i\n", tree_size, field[i].col[col]);
     }
   }
 
@@ -110,7 +113,7 @@ bool is_visable(Field *field, int row, int col) {
   for (int i = row + 1; i < NUM_ROWS; i++) {
     if (field[i].col[col] >= tree_size) {
       taller++;
-      printf("tree size: %i taller: %i\n", tree_size, field[i].col[col]);
+      // printf("tree size: %i taller: %i\n", tree_size, field[i].col[col]);
     }
   }
 
--- test.c
+++ test.c
@@ -1,19 +1,49 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+#define NUM_ROWS 99
+
+typedef struct Field {
+  int col[NUM_ROWS];
+} Field;
+
+bool is_visable(Field *field, int row, int col);
+
+int main(void) {
   FILE *file;
   if ((file = fopen("inputs/input8.txt", "r")) == NULL) {
     printf("Unable to open file.\n");
     return -1;
   }
+  Field *row;
+  if ((row = malloc(sizeof(Field) * NUM_ROWS)) == NULL) {
+    printf("Malloc failed.\n");
+    return -1;
+  }
 
-  char c;
-  int line_num = 1;
-  printf("%d: ", line_num++);
+  char c = 0;
+  int indx1 = 0;
+  int indx2 = 0;
+
+  while ((c = fgetc(file)) != EOF) {
+    if (c >= '0' && c <= '9') {
+      // Place tree height into relative col index, then increment indx2, until
+      // end of line.
+      row[indx1].col[indx2++] = atoi(&c);
+    } else if (c == '\n') {
+      indx1++;   // Icrement indx1 to move to next row,
+      indx2 = 0; // and reset indx2 to start at beginning of col.
+    }
+  }
+
+  int line_num = 0;
   while ((c = fgetc(file)) != EOF) {
     printf("%c", c);
     if (c == '\n') {
       printf("%d: ", line_num++);
     }
   }
+
+  return 0;
 }
