40f5ed13c2c749ea7be757e24ad687229a612b8f
Koss86 <jacobdelarosa123@gmail.com>
2025-04-28 05:07:58 -0500

    started day7, no idea what im doing though

--- day7.c
+++ day7.c
@@ -2,14 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Dir {
+  char *name;
+  struct Dir *parent;
+} Dir;
+typedef struct Filesystem {
+  struct Dir dirs[210];
+}
+
 int main() {
   FILE *file;
   if ((file = fopen("inputs/input7.txt", "r")) == NULL) {
     printf("Unable to open file.\n");
     return -1;
   }
-  char buffer[256];
-  memset(buffer, 0, 256 * sizeof(char));
+  char buffer[32];
+  memset(buffer, 0, 32 * sizeof(char));
 
+  int dirs = 0;
+  int indx = 0;
+  char c;
+  while ((c = fgetc(file)) != EOF) {
+    if (c != '\n') {
+      buffer[indx++] = c;
+    } else {
+      if (buffer[0] == 'd' && buffer[2] == 'r' && buffer[3] == ' ') {
+        dirs++;
+      }
+      indx = 0;
+    }
+  }
+  printf("%i\n", dirs);
   return 0;
 }
