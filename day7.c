90b185766887311696ca5794a145582f2395ecab
Koss86 <jacobdelarosa123@gmail.com>
2025-04-28 23:09:48 -0500

    misc

--- day7.c
+++ day7.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct File {
+  char *name;
+  int size;
+  struct Dir *file_parent;
+} File;
+
 typedef struct Dir {
   char *name;
-  struct Dir *parent;
+  int size;
+  File *files[100];
+  struct Dir *dir_parent;
 } Dir;
 
 typedef struct Filesystem {
-  struct Dir dirs[210];
+  struct Dir dir[210];
 } Filesystem;
 
 int main() {
@@ -17,24 +25,24 @@ int main() {
     printf("Unable to open file.\n");
     return -1;
   }
+  Filesystem *filesystem;
+  if ((filesystem = malloc(sizeof(Filesystem))) == NULL) {
+    printf("Error. Malloc failed for Filesystem.\n");
+    return -1;
+  }
+
   char buffer[32];
   memset(buffer, 0, 32 * sizeof(char));
-
   int dirs = 0;
   int indx = 0;
   char c;
+
   while ((c = fgetc(file)) != EOF) {
     if (c != '\n') {
       buffer[indx++] = c;
     } else {
-      buffer[indx] = '\0';
-      if (buffer[0] == 'd' && buffer[2] == 'r' && buffer[3] == ' ') {
-        dirs++;
-      }
-      indx = 0;
-      printf("%s\n", buffer);
     }
   }
-  printf("%i\n", dirs);
+
   return 0;
 }
