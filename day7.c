36a009fcea71536d28d4569f17a0a54451fac7ce
Koss86 <jacobdelarosa123@gmail.com>
2025-07-01 04:45:34 -0500

    Day 7
    Trying to figure out what kinda data struct I'm going to need.

--- day7.c
+++ day7.c
@@ -2,27 +2,44 @@
 // #include <stdlib.h>
 #include <string.h>
 
+typedef struct Dir {
+    struct Dir *parent;
+    char name[32];
+    int numofdirs;
+    int sizeofdirs;
+    int numoffiles;
+    int sizeoffies;
+    int dirsize;
+    struct Dir **dirlist;
+} Dir;
+
 int main() {
-  FILE *file;
-  if ((file = fopen("inputs/input7.txt", "r")) == NULL) {
-    printf("Unable to open file.\n");
-    return -1;
-  }
-
-  char buffer[32];
-  memset(buffer, 0, 32 * sizeof(char));
-  int indx = 0;
-  char c;
-
-  while ((c = fgetc(file)) != EOF) {
-    if (c != '\n') {
-      buffer[indx++] = c;
-    } else {
-      buffer[indx] = '\0';
-      indx = 0;
+    FILE *file;
+    if ((file = fopen("inputs/input7.txt", "r")) == NULL) {
+        printf("Unable to open file.\n");
+        return -1;
     }
-  }
 
-  fclose(file);
-  return 0;
+    char buffer[1200];
+    memset(buffer, 0, 1200 * sizeof(char));
+    int indx = 0;
+    char c;
+
+    long ct;
+    while ((c = fgetc(file)) != EOF) {
+        if (c != '\n') {
+            buffer[indx++] = c;
+        } else {
+            buffer[indx] = '\0';
+            indx = 0;
+        }
+        ct++;
+
+    }
+
+printf("%li chars.\n", ct);
+
+
+    fclose(file);
+    return 0;
 }
